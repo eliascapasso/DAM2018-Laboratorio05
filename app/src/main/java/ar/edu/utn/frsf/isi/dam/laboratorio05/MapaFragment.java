c0e99942f4d5a39f5a3034bd4d76d85dd0f45115
JaquelinaAcosta <jaque-acosta@hotmail.com>
2018-12-14 11:06:50 -0300

    Lab05- Req03 - terminado (andando)

--- app/src/main/java/ar/edu/utn/frsf/isi/dam/laboratorio05/MapaFragment.java
+++ app/src/main/java/ar/edu/utn/frsf/isi/dam/laboratorio05/MapaFragment.java
@@ -2,26 +2,36 @@ package ar.edu.utn.frsf.isi.dam.laboratorio05;
 
 
 import android.Manifest;
-import android.content.Context;
+
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.utn.frsf.isi.dam.laboratorio05.modelo.MyDatabase;
+import ar.edu.utn.frsf.isi.dam.laboratorio05.modelo.Reclamo;
+import ar.edu.utn.frsf.isi.dam.laboratorio05.modelo.ReclamoDao;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,9 +39,13 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapaFragment  extends SupportMapFragment implements OnMapReadyCallback {
 
     private static final int REQUEST_ACCESS_FINE_LOCATION = 1;
+    private static final int LISTA_RECLAMOS = 2;
     private static String[] PERMISSIONS_MAPS = {Manifest.permission.ACCESS_FINE_LOCATION};
     private GoogleMap miMapa;
-    private int tipoMapa;
+    private int tipoMapa=0;
+    private ReclamoDao reclamoDao;
+    private List<Reclamo> listaReclamos;
+
 
     private OnMapaListener listener;
 
@@ -49,10 +63,12 @@ public class MapaFragment  extends SupportMapFragment implements OnMapReadyCallb
     public MapaFragment() { }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup
-            container, Bundle savedInstanceState) {
-        View rootView = super.onCreateView(inflater, container,
-                savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        listaReclamos = new ArrayList<>();
+        reclamoDao = MyDatabase.getInstance(this.getActivity()).getReclamoDao();
+
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
         tipoMapa = 0;
         Bundle argumentos = getArguments();
         if(argumentos !=null) {
@@ -100,6 +116,19 @@ public class MapaFragment  extends SupportMapFragment implements OnMapReadyCallb
                     }
                 });
                 break;
+            case 2:
+                Runnable hiloCargarReclamos=new Runnable() {
+                    @Override
+                    public void run() {
+                        listaReclamos.clear();
+                        listaReclamos.addAll(reclamoDao.getAll());
+                        Message completeMessage= handler.obtainMessage(LISTA_RECLAMOS);
+                        completeMessage.sendToTarget();
+                    }
+                };
+                Thread thread= new Thread(hiloCargarReclamos);
+                thread.start();
+                break;
         }
     }
 
@@ -134,6 +163,31 @@ public class MapaFragment  extends SupportMapFragment implements OnMapReadyCallb
         }
     }
 
+
+    Handler handler= new Handler(){
+        @Override
+        public void handleMessage(Message mensaje) {
+            CameraUpdate cu;
+            LatLng latLng;
+            switch (mensaje.what){
+                case LISTA_RECLAMOS:
+                    ArrayList<MarkerOptions> marcadores= new ArrayList<>();
+                    for(int i=0; listaReclamos.size()>i; i++){
+                        latLng= new LatLng(listaReclamos.get(i).getLatitud(),
+                                listaReclamos.get(i).getLongitud());
+                        miMapa.addMarker(new MarkerOptions().position(latLng));
+                        marcadores.add(new MarkerOptions().position(latLng));
+                    }
+                    LatLngBounds.Builder builder= new LatLngBounds.Builder();
+                    for(MarkerOptions markerOptions: marcadores) builder.include(markerOptions.getPosition());
+                    LatLngBounds bounds= builder.build();
+                    cu= CameraUpdateFactory.newLatLngBounds(bounds, 0);
+                    miMapa.moveCamera(cu);
+                    break;
+
+            }
+        }
+    };
 
 }
 
